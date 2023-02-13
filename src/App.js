9ae6c13b5cbaf31988a8ff62d9f6040413fe5519
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-13 10:02:45 +0200

    navbar css added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar';
-
+import Home from './pages/Home';
+import { BrowserRouter as Router , Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+        <Router >
+        <Navbar />
+    <Routes >
+
+      <Route path="/" element={<Home/>}/>
+    
+    </Routes>
+
+ 
+        </Router>
+        
+      
+
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Logo from "../assets/logo.png";
-
+import { Link } from 'react-router-dom';
+import'../styles/Navbar.css';
+  
 function Navbar() {
   return (
     <div className='navbar'>
@@ -8,12 +10,15 @@ function Navbar() {
             <img src={Logo} />
         </div>
          <div className='rightSide'>
-
+            <Link to="/">Home</Link>
+            <Link to="/menu">Menu</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
          </div>
     
     </div>
 
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
