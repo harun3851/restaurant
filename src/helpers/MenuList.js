72c24c4316ecf2c2489756be3b716fd453e98177
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-13 13:30:04 +0200

    menu list added

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,22 @@
+import Pepperoni from"../assets/pepperoni.jpg";
+import Margherita from"../assets/margherita.jpg";
+import Vegan from"../assets/vegan.jpg";
+
+
+export const menuList=[
+    {
+        name:  "Pepperoni Pizza",
+        image: Pepperoni,
+        price: 12.00,
+    },
+    {
+        name:  "Margherita Pizza",
+        image: Margherita,
+        price: 12.97,
+    },
+    {
+        name:  "Vegan Pizza",
+        image: Vegan,
+        price: 12.90
+    }
+];
